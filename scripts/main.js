464bbbe657925af0b6624ab94d739bd848f6034d
Clint Cullen <clintc@koretech.com>
2023-09-14 16:43:08 -0500

    added id for js access. js updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,34 @@
-function multiply(num1, num2) {
-  let result = num1 * num2;
-  return result;
-}
-
-//myManifesto.innerHTML = `function multiply 100 * 30 = ${multiply(100, 30)}`;
-
-// document.querySelector("html").addEventListener("click", () => {
-//   alert("Ouch! Stop poking me!");
-// });
-
 const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
-    const mySrc = myImage.getAttribute("src");
-    if (mySrc === "images/Double-Striped-Bluet-200x300.jpg") {
-      myImage.setAttribute("src", "images/double-striped-bluet-2.jpg")
-    } else {
-      myImage.setAttribute("src", "images/Double-Striped-Bluet-200x300.jpg");
-    }
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/Double-Striped-Bluet-200x300.jpg") {
+    myImage.setAttribute("src", "images/double-striped-bluet-2.jpg")
+  } else {
+    myImage.setAttribute("src", "images/Double-Striped-Bluet-200x300.jpg");
+  }
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+}
