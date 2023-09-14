285db936499ceb525b329d7b6157f0848616168e
Clint Cullen <clintc@koretech.com>
2023-09-14 14:08:43 -0500

    initial commit of BlueDragonfly

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+function multiply(num1, num2) {
+  let result = num1 * num2;
+  return result;
+}
+
+myManifestoH3.innerHTML = `function multiply 100 * 30 = ${multiply(100, 30)}`;
+
+document.querySelector("html").addEventListener("click", () => {
+  alert("Ouch! Stop poking me!");
+});
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/Double-Striped-Bluet-200x300.jpg") {
+      myImage.setAttribute("src", "images/double-striped-bluet-2.jpg")
+    } else {
+      myImage.setAttribute("src", "images/Double-Striped-Bluet-200x300.jpg");
+    }
+};
